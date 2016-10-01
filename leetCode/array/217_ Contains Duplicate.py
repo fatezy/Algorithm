c4fd9463e0ccb5a2d75f0ef52cc0e8f0e21f94c2
fatezy <1104199203@qq.com>
2016-10-01 15:51:11 +0800

    Initial commit

---
+++ leetCode/array/217_ Contains Duplicate.py
@@ -0,0 +1,18 @@
+# Given an array of integers, find if the array contains any duplicates.
+# Your function should return true if any value appears at least twice in the array,
+#  and it should return false if every element is distinct.
+#判断是否有相同的项
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        map = {}
+        for i in range(len(nums)):
+            if nums[i] in map:
+                return True
+            else:
+                map[nums[i]] = 1
+
+        return False
