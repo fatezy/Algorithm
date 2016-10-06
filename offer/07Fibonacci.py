98dcb016b90af231b69c6058bc9f1185e3d256c1
fatezy <1104199203@qq.com>
2016-10-06 21:59:02 +0800

    6,7,8

---
+++ offer/07Fibonacci.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+# 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项。
+# n<=39
+class Solution:
+    def Fibonacci(self, n):
+        if n is 1:
+            return 1
+        if n is 2:
+            return 1
+        return self.Fibonacci(n-1)+self.Fibonacci(n-2)
+
+    def Fibonacci2(self, n):
+        count, a, b = 0, 0, 1
+        while count < n:
+            a, b = b, a + b
+            count += 1
+        return a
+
+
+
+if __name__ == '__main__':
+    print(Solution().Fibonacci(10))
---
+++ offer/08jumpFloor.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+# 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+class Solution:
+    def jumpFloor(self, number):
+        if number is 1:
+            return 1
+        if number is 2:
+            return 2
+        return self.jumpFloor(number-1)+self.jumpFloor(number-2)
+
+    def jumpFloor2(self, number):
+        count, a, b = 0, 0, 1
+        while count < number:
+            a, b = b, a + b
+            count += 1
+        return b
