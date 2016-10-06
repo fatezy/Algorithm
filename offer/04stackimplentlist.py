72ec332783b9d856f00676047f1c9dd9d883225a
fatezy <1104199203@qq.com>
2016-10-06 21:31:36 +0800

    4

---
+++ offer/04stackimplentlist.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+# 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+class Solution:
+    def __init__(self):
+        self.stackA = []
+        self.stackB = []
+
+    def push(self, node):
+        # write code here
+        self.stackA.append(node)
+
+    def pop(self):
+        # return xx
+        if self.stackB:
+            return self.stackB.pop()
+        elif not self.stackA:
+            return None
+        else:
+            while self.stackA:
+                self.stackB.append(self.stackA.pop())
+            return self.stackB.pop()
+
