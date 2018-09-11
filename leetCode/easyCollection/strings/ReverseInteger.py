ff24f80d4d4f2a95e946f3b3021f3c2ccfeb9527
fatezy <1104199203@qq.com>
2018-09-11 21:22:06 +0800

    数字翻转

---
+++ leetCode/easyCollection/strings/ReverseInteger.py
@@ -0,0 +1,25 @@
+#
+# 给定一个 32 位有符号整数，将整数中的数字进行反转。
+#
+# 示例 1:
+#
+# 输入: 123
+# 输出: 321
+#  示例 2:
+#
+# 输入: -123
+# 输出: -321
+# 示例 3:
+#
+# 输入: 120
+# 输出: 21
+
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sign = [1,-1][x < 0]
+        rst = sign * int(str(abs(x))[::-1])
+        return rst if -(2**31)-1 < rst < 2**31 else 0
