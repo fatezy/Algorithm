060b34aefcdfbfe29b74806f7d4b08bc6ea94f4f
fatezy <1104199203@qq.com>
2018-09-28 10:27:14 +0800

    13题重新排序

---
+++ offer/13reOrderArray.py
@@ -0,0 +1,24 @@
+# 题目描述
+# 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，
+# 所有的偶数位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+
+class Solution:
+    def reOrderArray(self, array):
+        even = []  # 用户存储偶数
+
+        temp = 0
+        for i in range(len(array)):
+            if array[i] % 2 == 0:
+                even.append(array[i])
+            else:
+                array[temp] = array[i]
+                temp += 1
+
+        array[temp:len(array)] = even[:]
+
+
+        return array
+
+
+if __name__ == '__main__':
+    print(Solution().reOrderArray(list(range(10))))
