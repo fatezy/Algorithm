6e23ac1e7d90dbb77457a0008e459710d23d711a
fatezy <1104199203@qq.com>
2018-10-01 14:55:16 +0800

    完善tree_helper

--- offer/tree_helper.py
+++ offer/tree_helper.py
@@ -3,3 +3,32 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
+
+
+
+
+class biTree:
+    def __init__(self,list):
+        self.list = list
+
+
+    def creatTree(self,root,i):
+        if i<len(self.list):
+            if self.list[i] =='#':
+                return None
+            else:
+                root = TreeNode(self.list[i])
+                root.left = self.creatTree(root.left,2*i+1)
+                root.right = self.creatTree(root.left,2*i+2)
+                return root
+        return root
+
+if __name__ == '__main__':
+    tree = biTree([1,2,3,'#',4,5])
+    root = tree.creatTree(None,0)
+    print(1)
+
+
+
+
+
