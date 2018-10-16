9b6cdec369c934a27697017ac5848cb8f281dcbd
fatezy <1104199203@qq.com>
2018-10-16 20:07:21 +0800

    24 tree_helper
    Signed-off-by: fatezy <1104199203@qq.com>

--- offer/tree_helper.py
+++ offer/tree_helper.py
@@ -23,14 +23,37 @@ class biTree:
                 return root
         return root
 
+    def levelOrder(self, root):
+        if not root:
+            return []
+
+        queue = [root]
+        results = []
+        while queue:
+            next_queue = []
+            results.extend([node.val for node in queue])
+            for node in queue:
+                if node.left:
+                    next_queue.append(node.left)
+
+                if node.right:
+                    next_queue.append(node.right)
+
+            queue = next_queue
+        return results
+
+
+
+
+
 
     def creat(self):
         return self.creatTree(None,0)
 
 if __name__ == '__main__':
-    tree = biTree([1,2,3,'#',4,5])
+    tree = biTree([1,2,3,'#',4,5,6])
     root = tree.creat()
-    print(1)
+    print(tree.levelOrder(root))
 
 
 
