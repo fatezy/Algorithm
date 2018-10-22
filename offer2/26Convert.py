55aea0dec3aa2482f2e600637a5f59d76792e7d4
fatezy <1104199203@qq.com>
2018-10-22 22:26:49 +0800

    26
    Signed-off-by: fatezy <1104199203@qq.com>

---
+++ offer2/26Convert.py
@@ -0,0 +1,27 @@
+# 题目描述
+# 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
+
+class Solution:
+    def Convert(self, pRootOfTree):
+        if not pRootOfTree:
+            return None
+        res = self.helper(pRootOfTree)
+        for i in range(len(res)):
+            if i != 0:
+                res[i].left =res[i-1]
+            if i != len(res)-1:
+                res[i].right = res[i+1]
+
+        return res[0]
+
+    def helper(self,pRootOfTree):
+        if not pRootOfTree:
+            return []
+
+        res = []
+        left = self.helper(pRootOfTree.left)
+        res = left + [pRootOfTree]
+        right = self.helper(pRootOfTree.right)
+        res = res + right
+
+        return res
