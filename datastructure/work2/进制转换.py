911bd50c67314a36726790c72053d3f4e5bef412
fatezy <1104199203@qq.com>
2018-10-25 18:15:14 +0800

    进制转换 递归写法
    Signed-off-by: fatezy <1104199203@qq.com>

--- datastructure/work2/进制转换.py
+++ datastructure/work2/进制转换.py
@@ -13,6 +13,20 @@ class Solution:
 
         return ''.join(str(x) for x in res[::-1])
 
+    def ten_to_eight2(self,num):
+        res = ''
+        if not num:
+            return res
+
+        if num // 8 == 0:
+            res += str(num)
+            return res
+
+        res +=  self.ten_to_eight2(num//8)
+        res += str(num % 8)
+        return res
+
+
 if __name__ == '__main__':
     print(Solution().ten_to_eight(1348))
-    print(Solution().ten_to_eight(234))
+    print(Solution().ten_to_eight2(1348))
