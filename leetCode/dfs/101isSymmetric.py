363ead83105d01b68ed92b1d0658e998ffec5c92
fatezy <1104199203@qq.com>
2018-12-11 10:15:08 +0800

    28
    Signed-off-by: fatezy <1104199203@qq.com>

---
+++ leetCode/dfs/101isSymmetric.py
@@ -0,0 +1,42 @@
+# 给定一个二叉树，检查它是否是镜像对称的。
+#
+# 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+#
+#     1
+#    / \
+#   2   2
+#  / \ / \
+# 3  4 4  3
+# 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+#
+#     1
+#    / \
+#   2   2
+#    \   \
+#    3    3
+
+class Solution:
+    def isSymmetric(self, root):
+        """判断二叉树是否为镜像二叉树，调用上题的是否为相同树的方法，中间的递归调用稍加修改即可
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.helper(root.left,root.right)
+
+
+    def helper(self,p,q):
+        if not p and not q:
+            return True
+        if p and q:
+            if p.val == q.val:
+                l = self.helper(p.left,q.right)
+                r = self.helper(p.right,q.left)
+                return l and r
+        return False
+
+
+
+
+
+if __name__ == '__main__':
+    print(123)
