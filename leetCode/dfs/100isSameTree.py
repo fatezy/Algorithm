ed1501615d89fce6d2a52c7839ed515c36b42134
fatezy <1104199203@qq.com>
2018-12-11 09:56:56 +0800

    100
    Signed-off-by: fatezy <1104199203@qq.com>

---
+++ leetCode/dfs/100isSameTree.py
@@ -0,0 +1,58 @@
+# 给定两个二叉树，编写一个函数来检验它们是否相同。
+#
+# 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+#
+# 示例 1:
+#
+# 输入:       1         1
+#           / \       / \
+#          2   3     2   3
+#
+#         [1,2,3],   [1,2,3]
+#
+# 输出: true
+# 示例 2:
+#
+# 输入:      1          1
+#           /           \
+#          2             2
+#
+#         [1,2],     [1,null,2]
+#
+# 输出: false
+# 示例 3:
+#
+# 输入:       1         1
+#           / \       / \
+#          2   1     1   2
+#
+#         [1,2,1],   [1,1,2]
+#
+# 输出: false
+from offer.tree_helper import *
+
+class Solution:
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        if p and q:
+            if p.val == q.val:
+                l = self.isSameTree(p.left,q.left)
+                r = self.isSameTree(p.right,q.right)
+                return l and r
+        return False
+
+
+if __name__ == '__main__':
+    tree = biTree([0,-5])
+    tree2 = biTree([0,-8])
+
+    p = tree.creat()
+    q = tree2.creat()
+    print(Solution().isSameTree(p, q))
+
