f7813f754931bcde45297daaba7f7f6f55da9a70
fatezy <1104199203@qq.com>
2019-01-07 23:40:06 +0800

    54
    Signed-off-by: fatezy <1104199203@qq.com>

---
+++ leetCode/top100/054spiralOrder.py
@@ -0,0 +1,46 @@
+# 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+#
+# 示例 1:
+#
+# 输入:
+# [
+#  [ 1, 2, 3 ],
+#  [ 4, 5, 6 ],
+#  [ 7, 8, 9 ]
+# ]
+# 输出: [1,2,3,6,9,8,7,4,5]
+# 示例 2:
+#
+# 输入:
+# [
+#   [1, 2, 3, 4],
+#   [5, 6, 7, 8],
+#   [9,10,11,12]
+# ]
+# 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        res = []
+        while matrix:
+            if matrix:
+                res.extend(matrix.pop(0)[::])
+            if matrix and matrix[0]:
+                res.extend([line.pop() for line in matrix])
+            if matrix:
+                res.extend(matrix.pop()[::-1])
+            if matrix and matrix[0]:
+                res.extend([line.pop(0) for line in matrix][::-1])
+
+        return res
+if __name__ == '__main__':
+    print(Solution().spiralOrder(
+        [[1, 2, 3, 4],
+         [5, 6, 7, 8],
+         [9, 10, 11, 12],
+         [13, 14, 15, 16]]
+))
